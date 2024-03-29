b0ed281eba4d73628814d0445a81fa77012858e4
VillalbaLeandro <exemprary@hotmail.com>
2024-03-29 05:02:14 -0300

    Se agrego boton y funcionalidad de descarga cv

--- app/components/AboutMe.jsx
+++ app/components/AboutMe.jsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import React from 'react'
+import DownloadButton from './DownloadButton'
 
 const AboutMe = () => {
     return (
-        <div className='text-center  glass-effect rounded xs:max-w-80 xl:max-w-96 flex flex-col gap-1  p-10' >
-            <Image className='translate-y-10 ' src='/img/myPics/me2.png' alt='My beatiful picture' width={720} height={720} />
+        <div className='text-center glass-effect rounded xs:max-w-80 xl:max-w-96 flex items-center flex-col gap-1  p-10' >
+            <Image className='translate-y-10  ' src='/img/myPics/me2.png' alt='My beatiful picture' width={300} height={300} />
             <h1 id="about-me" className='text-gray-100 font-bold text-base dark:text-gray-600'>Hola!  👋🏻 soy Leandro Villalba y soy</h1>
             <h2 className='font-bold text-5xl  '>
                 FRONTEND  </h2>
@@ -14,6 +15,7 @@ const AboutMe = () => {
             <h2 className='font-bold text-3xl'>Analista de Sistemas</h2>
             <p className='text-gray-100 text-sm  dark:text-gray-600'>
 Soy un desarrollador Frontend con más de 2 años de experiencia, especializado en encontrar soluciones innovadoras y eficientes. Destaco por mi capacidad para identificar rápidamente problemas y ofrecer soluciones creativas, mejorando la experiencia del usuario y la eficiencia operativa. Siempre aprendiendo y al tanto de las últimas tendencias en tecnología, mi objetivo es seguir creciendo profesionalmente y contribuir al éxito de proyectos innovadores mientras sigo explorando nuevas tecnologías.</p>
+            <DownloadButton />
             <div id='experience'></div>
 
         </div>
